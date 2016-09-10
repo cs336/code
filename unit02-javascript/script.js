7f23753c4e05fd6001d395ddb61c68bc416114fe
kvlinden <kvlinden@calvin.edu>
2016-09-10 15:41:08 -0400

    prepped unit 2 code...

--- unit02-javascript/script.js
+++ unit02-javascript/script.js
@@ -1,12 +1,145 @@
 /**
- * Sample code for CS 336, Unit 2 - Run with either:
- *    node script.js
- *    Chrome, URL: file:///home/cs336/projects/fundamentals/unit02-javascript/index.html
- *        See the Developertools console output.
- * Node and JSLint must be installed.
+ * This implements some JS constructs, including first-class objects and prototype-base OO.
  *
  * @author kvlinden
  * @version summer2016
  */
 
-console.log("Hello, console!");
+// 1. ----------------------------------
+// Looseness: optional/dynamic typing
+var test;
+test = 42;
+test = "forty-two";
+test = true;
+console.log(test);
+
+
+
+// 2. ----------------------------------
+// Sameness
+console.log(1 == "1");
+console.log(1 === "1");
+console.log(1 == true);
+console.log(1 === true);
+
+
+
+// 3. ----------------------------------
+// Functions as first-class objects
+function greetMe(names) {
+    for (name of names) {
+        console.log("Hello, " + name + "!");
+    }
+}
+let greetMe2 = function(names) {
+    for (name of names) {
+        console.log("Hello, " + name + "!");
+    }
+}
+greetMe(["world", "galaxy", "universe"]);
+greetMe2(["world", "galaxy", "universe"]);
+
+
+
+// 4. ----------------------------------
+// Anonymous functions
+function runThis(aFunction, args) {
+    aFunction.call(null, args);
+}
+runThis(function(names) { for (name of names) { console.log("Hello, " + name + "!"); }},
+        ["world", "galaxy", "universe"]);
+
+
+
+// 5. ----------------------------------
+// A common use cases for anonymous functions.
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+var myList = [1,2,3].map(function(x) {return x*2;});
+//var myList = [1,2,3].map(x => x*2);
+console.log(myList);
+
+
+
+// 6. ----------------------------------
+// Closures
+function makeGreeter(salutation) {
+    return function(names) {
+        for (name of names) {
+            console.log(salutation + name + "!");
+        }
+    }
+}
+var friendlyGreeter = makeGreeter("Hello, ");
+var unfriendlyGreeter = makeGreeter("Get lost, ");
+friendlyGreeter(["world", "galaxy", "universe"]);
+unfriendlyGreeter(["world", "galaxy", "universe"]);
+
+// 7. ----------------------------------
+// Strange closure example
+//   from http://stackoverflow.com/questions/111102/how-do-javascript-closures-work - example 5
+function buildListOfFunctions(list) {
+    var result = [];
+    for (var i = 0; i < list.length; i++) {
+        result.push( function() {console.log(i + ': ' + list[i])} );
+    }
+    return result;
+}
+for (f of buildListOfFunctions([1,2,3])) {
+    f();
+}
+
+
+
+// Object-oriented programming in JavaScript
+
+// 8. ----------------------------------
+// Encapsulation
+function Shape(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Shape.prototype.move = function(newX, newY) {
+    this.x = newX;
+    this.y = newY;
+}
+var s1 = new Shape(0, 0);
+console.log(s1);
+var s2 = new Shape(1, 2);
+s2.move(2, 3);
+console.log(s2);
+
+
+
+// 9. ----------------------------------
+// Inheritance
+function Rectangle(x, y, width, height) {
+    Shape.call(this, x, y);
+    this.width = width;
+    this.height = height;
+}
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.area = function() {
+    return this.width * this.height;
+}
+var r1 = new Rectangle(1, 2, 1, 1);
+console.log(r1);
+console.log(r1.area());
+r1.move(2, 3);
+console.log(r1);
+
+
+
+// 10. ----------------------------------
+// Polymorphism
+function Circle(x, y, radius) {
+    Shape.call(this, x, y);
+    this.radius = radius;
+}
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.area = function() {
+    return Math.PI * this.radius * this.radius;
+}
+var c1 = new Circle(1, 2, 1);
+console.log(c1.area());
