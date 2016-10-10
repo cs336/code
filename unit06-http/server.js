d39d0cf379495cd3167a0d3e8b53f7e21f40777b
kvlinden <kvlinden@calvin.edu>
2016-10-10 11:52:01 -0400

    added route, form and cookie examples to unit 6...

--- unit06-http/server.js
+++ unit06-http/server.js
@@ -1,5 +1,5 @@
 /**
- * This implements some HTTP cookie and form exercises.
+ * This implements some HTTP method/code, form and cookie examples.
  *
  * @author kvlinden
  * @version summer2016
@@ -20,27 +20,51 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+// --------------------------------
+// HTTP route and return code examples.
+
 // This responds to GET requests for a hello message.
-app.get('/hello', function(req, res) {
+app.get('/', function(req, res) {
     res.send("Hello, Express route!");
 });
 
+// This rejects all access to the secret route.
+app.get('/secret', function(req, res) {
+    res.sendStatus(http_status.FORBIDDEN);
+});
+
+
+// --------------------------------
+// HTTP form example
+
+// Responds to form posts from the forms/index.html example.
+app.post('/forms', function(req, res) {
+    res.send('Hello, form POST!<br>Posted message: <code>'
+	     + req.body.user_message + '</code>');
+});
+
+
+// --------------------------------
+// HTTP cookies examples
+
 // This implements routes to list/create/delete cookies.
-app.get("/", function(req, res) {
+app.get("/cookies", function(req, res) {
     let cookieMessage = 'cookieName not set...';
     if (req.cookies.cookieName) {
         cookieMessage = "cookieName: " + req.cookies.cookieName;
     }
     res.send("Hello, cookies!<br> " + cookieMessage);
 });
-app.get("/set", function(req, res) {
+app.get("/cookies/set", function(req, res) {
     res.cookie("cookieName", "cookieValue")
     res.send("Cookie is set.");
 });
-app.get("/clear", function(req, res) {
+app.get("/cookies/clear", function(req, res) {
     res.clearCookie("cookieName");
     res.send("Cookie is deleted.");
 });
+
 
 app.listen(PORT, HOST, () => {
     console.log("listening on " + HOST + ":" + PORT + "...");
