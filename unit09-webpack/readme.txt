7ef4bc77f7223cae558b085ddbea1661b18ddb17
kvlinden <kvlinden@calvin.edu>
2016-11-03 13:52:31 -0400

    completed lab 9.1...

--- unit09-webpack/readme.txt
+++ unit09-webpack/readme.txt
@@ -1,7 +1,1 @@
-This is a refactored implementation of the (old) Facebook ReactJS tutorial - the comment list:
-
-     https://cs.calvin.edu/courses/cs/336/kvlinden/08react/backup/Tutorial_React.html
-     (formerly at https://facebook.github.io/react/docs/tutorial.html)
-
-This application is modularized and bundled using WebPack based on the specification in lab exercise 9 (https://cs.calvin.edu/courses/cs/336/kvlinden/09webpack/lab.html).
-
+This application implements lab exercise 9.1 (see https://cs.calvin.edu/courses/cs/336/kvlinden/09webpack/lab.html).
--- unit09-webpack/webpack.config.js
+++ unit09-webpack/webpack.config.js
@@ -1,6 +1,3 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-
 module.exports = {
     entry: [
         __dirname + '/app/scripts/index.js'
@@ -14,17 +11,6 @@ module.exports = {
             { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
             { test: /\.css$/,  loader: 'style!css?modules!postcss' }
         ]
-    },
-    plugins: [
-	new HtmlWebpackPlugin({template: __dirname + "/app/index.tmpl.html"}),
-	new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
-        port: 3001,
-        proxy: { '/api/*': 'http://localhost:3000' },
-        colors: true,
-        historyApiFallback: true,
-        inline: true,
-        hot: true
     }
 };
+
