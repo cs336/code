2cf7ca70b214440a390363767ef0e38f58d1b349
kvlinden <kvlinden@calvin.edu>
2016-11-24 18:21:38 -0500

    prepped lab13...

---
+++ app/scripts/commentEdit.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router';
+import $ from 'jquery';
+
+import { API_URL } from './global';
+import { store, ActionTools, StoreTools } from './flux';
+
+module.exports = React.createClass({
+    getInitialState: function() {
+        return {author: '', text: ''};
+    },
+    componentDidMount: function() {
+        let commentToEdit = StoreTools.findComment(this.props.params.id, store.getState().data);
+        this.setState({author: commentToEdit.author, text: commentToEdit.text});
+    },
+    handleAuthorChange: function(e) {
+        this.setState({author: e.target.value});
+    },
+    handleTextChange: function(e) {
+        this.setState({text: e.target.value});
+    },
+    contextTypes: {
+        router: React.PropTypes.object
+    },
+    handleUpdate: function() {
+        var updatedComment = {
+	    id: Number(this.props.params.id),
+            author: this.state.author.trim(),
+            text: this.state.text.trim()
+        }
+        store.dispatch(ActionTools.editComment(updatedComment));
+        this.context.router.push('/');
+    },
+    render: function() {
+        return (
+            <div>
+                <form className="commentForm">
+                    <h1>Comment Edit - {this.state.id}</h1>
+                    <input
+                        type="text"
+                        value={this.state.author}
+                        onChange={this.handleAuthorChange}
+                    />
+                    <input
+                        type="text"
+                        value={this.state.text}
+                        onChange={this.handleTextChange}
+                    />
+                    <button type="button" onClick={this.handleUpdate}>Update</button>
+                    <button type="button" onClick={this.handleDelete}>Delete</button>
+                </form>
+                <Link to='/'>Cancel</Link>
+            </div>
+        );
+    }
+});
--- readme.txt
+++ readme.txt
@@ -1,1 +1,6 @@
-Keep calm and watch this space.
+This is a refactored implementation of the (old) Facebook ReactJS tutorial - the comment list:
+
+     https://cs.calvin.edu/courses/cs/336/kvlinden/08react/backup/Tutorial_React.html
+     (formerly at https://facebook.github.io/react/docs/tutorial.html)
+
+This application is upgraded to integrate Redux as specified in lab exercise 13 (https://cs.calvin.edu/courses/cs/336/kvlinden/13redux/lab.html). For the running app, see https://comments-kvlinden.herokuapp.com/.
